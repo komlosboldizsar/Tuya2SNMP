aece1a992e48fe11b54312f456d78813c1649396
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-08-08 15:11:27 +0200

    Add all important DPs for Miboxer FUT037W

--- Tuya2SNMP/SnmpAdapters/Miboxer/MiboxerFUT037W.cs
+++ Tuya2SNMP/SnmpAdapters/Miboxer/MiboxerFUT037W.cs
@@ -32,12 +32,10 @@ namespace Tuya2SNMP.SnmpAdapters.MiboxerFUTW
             new SimpleEnumDataProvider(DP_WORKMODE, workmodes, -1),
             new SimpleIntDataProvider(DP_BRIGHT, -1, 10, 1000),
             new SimpleIntDataProvider(DP_TEMP, -1, 0, 1000),
-            new SimpleHexStringDataProvider(DP_COLOUR, new HexStringDataProvider.HexStringDescriptor[]
-            {
-                new(4, 0, 359),
-                new(4, 10, 1000),
-                new(4, 10, 1000)
-            })
+            new SimpleHexStringDataProvider(DP_COLOUR, colourHexStringDescriptors),
+            new HexPartCalcIntDataProvider(CALC_COLOUR_HUE, DP_COLOUR, colourHexStringDescriptors, 0),
+            new HexPartCalcIntDataProvider(CALC_COLOUR_SATURATION, DP_COLOUR, colourHexStringDescriptors, 1),
+            new HexPartCalcIntDataProvider(CALC_COLOUR_BRIGHTNESS, DP_COLOUR, colourHexStringDescriptors, 2)
         };
 
         public const int DP_SWITCH = 20;
@@ -45,6 +43,9 @@ namespace Tuya2SNMP.SnmpAdapters.MiboxerFUTW
         public const int DP_BRIGHT = 22;
         public const int DP_TEMP = 23;
         public const int DP_COLOUR = 24;
+        public const int CALC_COLOUR_HUE = 10001;
+        public const int CALC_COLOUR_SATURATION = 10002;
+        public const int CALC_COLOUR_BRIGHTNESS = 10003;
 
         private readonly Dictionary<string, int> workmodes = new()
         {
@@ -52,6 +53,13 @@ namespace Tuya2SNMP.SnmpAdapters.MiboxerFUTW
             { "colour", 1 },
             { "scene", 2 },
             { "music", 3 },
+        };
+
+        private readonly HexStringDescriptor[] colourHexStringDescriptors = new HexStringDescriptor[]
+        {
+            new(4, 0, 359),
+            new(4, 0, 1000),
+            new(4, 10, 1000)
         };
 
     }
