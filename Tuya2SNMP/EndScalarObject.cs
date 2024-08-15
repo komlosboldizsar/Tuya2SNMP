17a2e4fbcd9380aed154d6c2eeeaef866555e881
Komlos Boldizsar <komlos.boldizsar@gmail.com>
2024-08-15 08:57:50 +0200

    Fix: add "closer" SNMP object so last real object can be also queried

---
+++ Tuya2SNMP/EndScalarObject.cs
@@ -0,0 +1,20 @@
+﻿using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Pipeline;
+
+namespace Tuya2SNMP
+{
+    internal class EndScalarObject : ScalarObject
+    {
+
+        public EndScalarObject()
+            : base(new ObjectIdentifier("1.3.9999"))
+        { }
+
+        public override ISnmpData Data
+        {
+            get => new Integer32(0);
+            set { }
+        }
+
+    }
+}
